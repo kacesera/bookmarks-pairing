41f7468d3bcbed14fee54f886778bc436300ab15
KC Thomas <thisiskcthomas@gmail.com>
2020-10-23 18:00:57 +0100

    Fixed 1 bug in 'delete_bookmark_spec' feature test, and fixed a syntax error in the HTML in bookmarks.erb.

--- spec/features/delete_bookmark_spec.rb
+++ spec/features/delete_bookmark_spec.rb
@@ -1,9 +1,10 @@
 feature 'Delete Bookmarks' do
   scenario "User can delete bookmarks from '/bookmarks' path" do 
-    test_connection
     add_bookmark_for_deletion
     visit '/bookmarks'
     first('.bookmark').click_button 'Delete'
+
+    expect(current_path).to eq '/bookmarks'
     expect(page).to_not have_link('Facebook', href: 'www.facebook.com')
   end
 end
